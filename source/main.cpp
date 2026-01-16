b51e214e7f789129f5d046f3c28c5f6340ef8f3e
Troyzhen <tropicboytroy@gmail.com>
2026-01-16 01:45:46 -0500

    removed windows paint to window, prep for directx 11

--- source/main.cpp
+++ source/main.cpp
@@ -1,7 +1,15 @@
-#define WIN32_MEAN_AND_LEAN
+#include <cstddef>
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 #include <tchar.h>
-#include <string>
+#include <d3d11.h>
+#include <dxgi.h>
+// #include <string>
+// #include "terminal_colors.h"
+
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"dxgi.lib")
+
 
 
 
@@ -10,7 +18,9 @@ const int wHeight = 800;
 const TCHAR* WndClassName = TEXT("Window Blueprint");
 const TCHAR* WndTitle = TEXT("Dxrend");
 
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -47,51 +57,50 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     ShowWindow(hWnd, nCmdShow);
     UpdateWindow(hWnd);
 
+    // initialize d3d
+
+
+
     MSG msg = {};
     bool running = true;
     while (running)
     {
-       while(PeekMessage(&msg , NULL, 0, 0, PM_REMOVE))
-       {
-           if(msg.message == WM_QUIT)
-           {
-               running = false;
-           }
-
-           TranslateMessage(&msg);
-           DispatchMessage(&msg);
-       }
-
-       // game loop code
-
+        // Handle messages
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            if (msg.message == WM_QUIT)
+                running = false;
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
     }
+
 
     return  msg.wParam;
 }
 
+
 // handle messages within the procedure function
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
-    const TCHAR* greeting = TEXT("Hello Windows Desktop!");
 
     switch (message)
     {
         case WM_PAINT:
-            hdc = BeginPaint(hWnd, &ps);
-
-            TextOut(hdc, wWidth/2, wHeight/2, greeting, _tcslen(greeting));
-
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // Directx will paunt, so no need to paint here
             EndPaint(hWnd, &ps);
             break;
+        }
 
         case WM_DESTROY:
             PostQuitMessage(0);
             break;
 
-        case WM_KEYDOWN: // WM_CHAR is for text inputWM_KEYDOWN is for key presses like WASD
-            if (wParam == 'C') // when C key is pressed the window title changes
+        case WM_KEYDOWN: // WM_CHAR is for text input WM_KEYDOWN is for key presses like WASD
+            if (wParam == 'C')
             {
                 SetWindowText(hWnd, "New Window Name");
             }
